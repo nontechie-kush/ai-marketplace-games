568e3569fd8ebbb4c26d3aa0f62e7f607d356d26
nontechie-kush <kushchatgpt@gmail.com>
2025-08-23 17:47:24 +0530

    Update route.js

--- app/api/compile/[gameId]/route.js
+++ app/api/compile/[gameId]/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
-import { supabaseAdmin } from '../../../../../lib/supabaseAdmin'
-import { compile as compileFromSpec } from '../../../../../lib/compiler'
+import { supabaseAdmin } from '../../../../lib/supabaseAdmin'
+import { compile as compileFromSpec } from '../../../../lib/compiler'
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
 const SUPABASE_ANON = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
