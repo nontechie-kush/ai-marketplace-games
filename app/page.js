a9872f36c5533b1fa1781f19662799fefe993d1e
nontechie-kush <kushchatgpt@gmail.com>
2025-08-25 19:02:48 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 export const dynamic = 'force-dynamic';
 
 import Link from 'next/link'
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../lib/supabase';
 
 // utils
 function fmt(n) {
